83e43609de16450250ee8dc5e93d5282539db685
Abraham De La Hoya <abrahamadlha@gmail.com>
2025-05-23 15:53:02 -0600

    Phone done

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ document.getElementById("selector").addEventListener("change", function() {
     const opcionSeleccionada = this.value;
     document.getElementById(opcionSeleccionada).style.display = "block";
 });
+
+document.getElementById("selector2").addEventListener("change", function() {
+    // Oculta todos los divs de contenido
+    document.querySelectorAll(".contenido-opcion2").forEach(function(div) {
+        div.style.display = "none";
+    });
+    
+    // Muestra solo el div seleccionado
+    const opcionSeleccionada = this.value;
+    document.getElementById(opcionSeleccionada).style.display = "block";
+});
